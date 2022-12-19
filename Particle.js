c6b36217b312b128ea292aed7ad26d8a88b4ca36
namu0309 <115522670+namu0309@users.noreply.github.com>
2022-12-20 01:45:59 +0900

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -25,6 +25,9 @@ class Particle {
     if (this.pos.y > height) {
       this.vel.y = this.vel.y * -0.8;
       this.pos.y = height;
+      if (this.pos.y > height) {
+        translate(randam(-5,5),random(-5,5))
+      }
     }
     
     if (this.pos.x > width) {
